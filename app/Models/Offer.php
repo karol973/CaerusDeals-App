18486cabe5249c511460b398a6553573626f9254
Karol Przybysz <k.przybysz@samsung.com>
2025-09-26 15:25:26 +0200

    Add company and products relation

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -9,4 +9,12 @@ class Offer extends Model
 {
     /** @use HasFactory<\Database\Factories\OfferFactory> */
     use HasFactory;
+
+    public function company(){
+        return $this->belongsTo(Company::class, 'company_id');
+    }
+
+    public function products(){
+        return $this->belongsToMany(Product::class, 'productsoffers', 'offer_id','product_id');
+    }
 }
